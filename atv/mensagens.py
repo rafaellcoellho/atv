4590b54d80e4a6b2cfa34e91c98446fe3f8ad946
Rafael Coelho <rafaellcoellho@gmail.com>
2022-11-07 14:15:39 -0300

    remove ponto final de mensagens

--- atv/mensagens.py
+++ atv/mensagens.py
@@ -3,9 +3,9 @@ from enum import Enum
 
 class Mensagens(Enum):
     ERRO_DESCRICAO_NAO_PODE_SER_VAZIA = (
-        "Atividade não adicionada, descrição não pode ser vazia."
+        "Atividade não adicionada, descrição não pode ser vazia"
     )
-    ERRO_COMANDO_NAO_IMPLEMENTADO = "Comando não foi implementado."
+    ERRO_COMANDO_NAO_IMPLEMENTADO = "Comando não foi implementado"
     ERRO_REMOVER_ATIVIDADE_INEXISTENTE = (
         "A atividade nesse indice não existe para ser removida"
     )
@@ -15,7 +15,7 @@ class Mensagens(Enum):
     ERRO_DESFAZER_ATIVIDADE_INEXISTENTE = (
         "A atividade nesse indice não existe para ser desfeita"
     )
-    SUCESSO_ADICIONAR_ATIVIDADE = "Atividade foi adiciona com sucesso."
+    SUCESSO_ADICIONAR_ATIVIDADE = "Atividade foi adiciona com sucesso"
     SUCESSO_REMOVER_ATIVIDADE = "Atividade foi removida com sucesso"
     SUCESSO_CONCLUIR_ATIVIDADE = "Atividade foi concluida com sucesso"
     SUCESSO_DESFAZER_ATIVIDADE = "Atividade foi desfeita com sucesso"
